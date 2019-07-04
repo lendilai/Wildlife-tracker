a77f155f2c519557757b0740955122d1b44c6d3b
lendilai <ngethenan768@gmail.com>
2019-07-04 15:33:38 +0300

    Add styling to all the pages and fix displays

--- create.sql
+++ create.sql
@@ -20,6 +20,6 @@ CREATE DATABASE wildlife_tracker;
 
  CREATE TABLE animals(id SERIAL PRIMARY KEY,the_name VARCHAR endangered boolean);
 
- CREATE TABLE sightings(id SERIAL PRIMARY KEY, ranger_id VARCHAR, species_id VARCHAR, animal_id VARCHAR,location_id VARCHAR, health VARCHAR, age VARCHAR, sighted_on VARCHAR);
+ CREATE TABLE sightings(id SERIAL PRIMARY KEY, ranger_id VARCHAR, species_id VARCHAR, animal_id VARCHAR,location_id VARCHAR, health VARCHAR, age VARCHAR, sighted_on timestamp);
 
  CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;
